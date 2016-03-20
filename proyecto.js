2ef29046f5f4324f190f6e8c0e81b3b98106c27d
Fabian Glenda <edfasebe1987@gmail.com>
2016-03-20 15:47:17 -0500

    errores commit JS

--- proyecto.js
+++ proyecto.js
@@ -1,3 +1,5 @@
+console.log("Inicio de programa");
 alert("Hola Mundo");
-document.write("<h1> BIENVENIDOS AL MUNDO JS </h1>");
-alert("Gracias por su visita");
+document.write("<h1> BIENVENIDOS AL MUNDO JS </h1>")
+alert("Gracias por su visita";
+console.log("Fin de Programa");
