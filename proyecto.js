4950415c3338bfd2e4bf03db233fd0e521f60a7f
javier peña <javierjunior198729@gmail.com>
2016-03-20 16:07:25 -0500

    corregido

--- proyecto.js
+++ proyecto.js
@@ -1,5 +1,6 @@
 console.log("Inicio de programa");
 alert("Hola Mundo");
-document.write("<h1> BIENVENIDOS AL MUNDO JS </h1>")
+document.write("<h1> BIENVENIDOS AL MUNDO JS </h1>");
 alert("Gracias por su visita";
+	alert("corregido colombia");
 console.log("Fin de Programa");
